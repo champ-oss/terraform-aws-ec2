1a7156850c4759f2973f980139299e822ff3674e
Phillip Langley <philliplangley@champmbplangley.lan>
2025-11-14 15:29:56 -0600

    output instance id

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,8 @@ output "ssm_manager_security_group_id" {
   description = "The ID of the security group used for SSM manager."
   value       = var.enabled && var.create_ssm_endpoint ? aws_security_group.ssm_manager[0].id : ""
 }
+
+output "instance_id" {
+  description = "The ID of the bastion host EC2 instance."
+  value       = var.enabled ? aws_instance.this[0].id : ""
+}
