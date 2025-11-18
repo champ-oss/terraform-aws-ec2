ed1c5b9ccbc74552ccee16236764c17de10ca580
Phillip Langley <philliplangley@champmbplangley.lan>
2025-11-18 12:45:14 -0600

    add extra ingress rules option

--- ec2-sg.tf
+++ ec2-sg.tf
@@ -3,11 +3,24 @@ resource "aws_security_group" "ec2" {
   name_prefix = var.git
   vpc_id      = var.vpc_id
 
-  # no inbound
+  # default egress
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # optional extra ingress rules
+  dynamic "ingress" {
+    for_each = var.extra_ingress_rules
+    content {
+      from_port       = ingress.value.from_port
+      to_port         = ingress.value.to_port
+      protocol        = ingress.value.protocol
+      cidr_blocks     = lookup(ingress.value, "cidr_blocks", null)
+      security_groups = lookup(ingress.value, "sg_ids", null)
+      description     = lookup(ingress.value, "description", null)
+    }
   }
 }
--- variables.tf
+++ variables.tf
@@ -111,3 +111,15 @@ variable "additional_ebs_volumes" {
   }))
   default = []
 }
+
+variable "extra_ingress_rules" {
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = optional(list(string), [])
+    sg_ids      = optional(list(string), [])
+    description = optional(string)
+  }))
+  default = []
+}
