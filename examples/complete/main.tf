77cd7ec0ccd4ac1ddfe1d22fa7ea844049e738e2
Phillip Langley <philliplangley@champmbplangley.lan>
2026-01-26 13:37:42 -0600

    removing data lookup

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -21,7 +21,12 @@ module "this" {
   private_subnet_ids    = data.aws_subnets.this.ids[0]
   vpc_id                = data.aws_vpcs.this.ids[0]
   ec2_user_data_script  = "ec2_user_data.sh"
-  ami_id                = "ami-0335b5183d7713fee" # Amazon Linux 2 AMI (HVM), SSD Volume Type
+  # execute aws describe images  \
+  #  --owners amazon \
+  #  --filters "Name=name,Values=amzn2-ami-hvm-*-x86_64-gp2" \
+  #  --query 'Images[0].ImageId' \
+  #  --output text to get ami id
+  ami_id                = "ami-0335b5183d7713fee"
   delete_on_termination = true
   additional_ebs_volumes = [
     {
